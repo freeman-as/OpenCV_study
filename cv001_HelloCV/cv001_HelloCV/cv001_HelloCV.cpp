b0099c4df1e26b3a14d277544f94139c32fd5bac
Atsunori Sagawa <freeman.by2018@gmail.com>
2019-01-07 23:47:41 +0900

    [Add] HelloCV first commit.

---
+++ cv001_HelloCV/cv001_HelloCV/cv001_HelloCV.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#pragma comment(lib, "opencv_world343d.lib")
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat img;
+	img = imread("image.jpg");
+	if (img.empty()) {
+		cout << "Unable to read the file";
+		cin.get();
+		return -1;
+	}
+	imshow("Image", img);
+	waitKey();
+	return 0;
+}
