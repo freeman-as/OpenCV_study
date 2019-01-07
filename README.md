35b19b2d25fb88b08b3328c6a667f68359877661
Atsunori Sagawa <freeman.by2018@gmail.com>
2019-01-08 08:03:15 +0900

    [Add] エッジ検出プログラム

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # OpenCV_study
+
+## PROJECT LIST
+
+- cv001_HelloCV 
+
+  OpenCVによる画像ファイルの読込と表示
+
+- cv002_EdgeDetection
+
+  Cannyアルゴリズムによるエッジ検出
---
+++ cv002_EdgeDetection/cv002_EdgeDetection/cv002_EdgeDetection.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include <opencv2/opencv.hpp>	// OpenCVヘッダ
+#pragma comment(lib, "opencv_world343d.lib")	// OpenCVライブラリ
+using namespace std;	// C++標準ライブラリ名前空間
+using namespace cv;		// OpenCV名前空間
+
+int main()
+{
+	Mat src = imread("../../data/lena.jpg", IMREAD_GRAYSCALE);
+	Mat	dest;
+
+	// Cannyアルゴリズムによるエッジ検出
+	Canny(src, dest, 50, 150);
+
+	imshow("入力画像", src);
+	imshow("出力画像", dest);
+	
+	// 出力画像の保存
+	imwrite("img_dest.png", dest);
+
+	waitKey();
+	return 0;
+}
